89f778d394d96417f1b28c7f7e9cdeba5e5231a7
alinuk1992 <ali.nourikhah@gmail.com>
2022-10-25 21:38:21 +0330

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero,
+        accusantium. Blanditiis iusto facere perferendis, illum nobis non
+        consequatur quam recusandae beatae possimus consequuntur explicabo
+        exercitationem soluta, alias assumenda deserunt, itaque enim similique
+        nesciunt magnam id quia? Saepe, eum nam? Sit.
+      </p>
+    </section>
+  );
+};
+
+export default About;
